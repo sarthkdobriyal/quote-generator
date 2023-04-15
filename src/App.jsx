4331d4d03a056bed84930dbd8face8e3dcbf9815
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-04-15 22:18:33 +0530

    initialized the app using vite and created components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=" text-center h-screen bg-gradient-to-tr from-purple-900 to-purple-300" >
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
