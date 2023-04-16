5d5b492bc3e8a6501af9a1460cd12d4e53febab5
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-04-16 06:59:46 +0530

    enhanced loaders - separated tags and quotes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Quote from '../components/Quote'
 import axios from 'axios'
 import Loader from '../components/Loader';
-import { useDispatch } from 'react-redux';
-import { add } from '../slices/bookmarkSlice';
 
 
 
@@ -13,38 +11,34 @@ const Home = () => {
     const [tags, setTags] = useState([]);
     const [tag, setTag] = useState("")
 
-    const [loading, setLoading] = useState(false);
+    const [quoteloading, setQuoteLoading] = useState(false);
+    const [tagloading, setTagLoading] = useState(false);
 
-    const dispatch = useDispatch();
 
     const fetchQuote = async () => {
-        setLoading(true)
+        setQuoteLoading(true)
             const res = await axios.get(`https://api.quotable.io/random?tags=${tag}`)
             setQuote(res.data);
-        setLoading(false);
+        setQuoteLoading(false);
     }
 
     const fetchTags = async () => {
-        setLoading(true)
+        setTagLoading(true)
         const res = await axios.get('https://api.quotable.io/tags');
         setTags(res.data);
-        setLoading(false);
+        setTagLoading(false);
     }
 
     useEffect(() => {
-
-        setLoading(true);
-        
         fetchTags();
         fetchQuote();
-        setLoading(false);
     },[])
 
 
   return (
     <div className="py-10 flex flex-col space-y-5 px-10 items-center" >
       {
-        loading ? <Loader /> :
+        quoteloading ? <Loader /> :
         
         <Quote
         id={quote._id}
@@ -55,7 +49,7 @@ const Home = () => {
 
 
         {
-            loading ? <Loader /> :
+            tagloading ? <Loader /> :
             <select name="tags" className='py-1 px-7 rounded-lg text-lg outline-none' value={tag} onChange={(e) => setTag(e.target.value)} >
             <option value="">Random</option>
             {
